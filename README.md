5de27b74f3cdf8f2ac00c9eac1526830ef5e392e
ri <git@tnu.jp>
2013-08-27 19:29:42 +0900

    ファイル構成変更、readme追加

---
+++ README.md
@@ -0,0 +1,4 @@
+- CategoryItem : カテゴリIDを指定してアイテムを取得するクラス
+- Item : 出品物の表現
+
+実行にはYahooのAPI KEYが必要、key.txtを読み込む
--- YAuction.rb
+++ YAuction.rb
@@ -2,39 +2,27 @@
 require './category_items.rb'
 require './item.rb'
 
-class YAuction
 
-  def self.testrun(apikey)
-    CategoryItems.set_api_key(apikey)
+def testrun(apikey)
+  CategoryItems.set_api_key(apikey)
 
-    cat = CategoryItems.new(2084193586,{min_price: 100, sort_by: :end_time, order: :desc})
-    cat.take(75).each_with_index do |val,i|
-      p i
-    end
-
-    puts "================="
-
-    cat.take(70).each_with_index do |val,i|
-      p i
-    end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  cat = CategoryItems.new(2084193586,{min_price: 100, sort_by: :end_time, order: :desc})
+  cat.take(75).each_with_index do |val,i|
+    p i
   end
 
+  puts "================="
+
+  cat.take(70).each_with_index do |val,i|
+    p i
+  end
 
 end
+
+def get_key_from_file(filename)
+  s = File.read(filename,encoding: Encoding::UTF_8)
+  s.chomp
+end
+
+key = get_key_from_file "key.txt"
+testrun(key)

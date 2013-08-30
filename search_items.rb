17c3f1d2806d39d90aa4fbed533d2baa370db25e
ri <git@tnu.jp>
2013-08-30 23:22:51 +0900

    SearchItemsの骨組とquery生成まで書いた

---
+++ search_items.rb
@@ -0,0 +1,124 @@
+# encoding:utf-8
+
+require 'open-uri'
+require 'nokogiri'
+require 'date'
+require './yahoo_api.rb'
+require './list_items.rb'
+
+class SearchItems < ListItems
+  include Enumerable
+  include YahooAPI
+
+  attr_reader :query
+  
+  def initialize(query,opt={})
+    super(opt)
+    @query = query
+  end
+
+  def create_request_url(page)
+    url = "http://auctions.yahooapis.jp/AuctionWebService/V2/search?" + 
+    "appid=#{@@api_key}" + 
+    "&query=#{@query}" + 
+    "&page=#{page}"
+
+    args = @options
+
+    category = args[:category_id] if args[:category_id].is_a?(Integer)
+
+    sort = case args[:sort_by]
+      when :end_time
+        "end"
+      when :has_image
+        "img"
+      when :bids
+        "bids"
+      when :current_price
+        "cbids"
+      when :buy_price
+        "bidorbuy"
+      when :affiliate
+        "affilate"
+      else 
+        nil
+      end
+
+    order = case args[:order]
+      when :desc
+        "d"
+      when :asc 
+        "a"
+      else 
+        nil
+      end
+
+    store = case args[:store]
+      when :all
+        0
+      when :store
+        1
+      when :normal
+        2
+      else 
+        nil
+      end
+
+    item_status = case args[:item_status]
+      when :all
+        0
+      when :new
+        1
+      when :used
+        2
+      else 
+        nil
+      end
+
+    aucmin_bidorbuy_price = args[:min_buy_price] if args[:min_buy_price].is_a?(Integer)
+    aucmax_bidorbuy_price = args[:max_buy_price] if args[:max_buy_price].is_a?(Integer)
+
+    aucmaxprice = args[:max_price] if args[:max_price].is_a?(Integer)
+    aucminprice = args[:min_price] if args[:min_price].is_a?(Integer)
+
+    buynow = case args[:buynow]
+      when true
+        1
+      when false
+        2
+      else 
+        nil
+      end
+
+    f = case args[:search_target]
+        when :only__title
+          0x8
+        when :title_and_text
+          0x4
+        when :title_and_keyword
+          0x2
+        else
+          nil
+        end
+
+    %w(buynow aucmaxprice aucminpricee sort order store item_status aucmin_bidorbuy_price aucmax_bidorbuy_price f category).each do |a|
+      url += "&#{a}=#{eval(a)}" if eval(a)
+    end
+
+
+
+
+    return url
+  end
+
+
+
+
+  def get_item_list(url)
+
+
+
+  end
+
+
+

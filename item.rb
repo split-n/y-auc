9634e1cf8e27a7a492ab551cfb07abc79a038b08
ri <git@tnu.jp>
2013-09-05 20:42:43 +0900

    procベースで取得を作成中(not working)

--- item.rb
+++ item.rb
@@ -6,25 +6,58 @@ require 'nokogiri'
 class Item
   include YahooAPI
   TAGS_TABLE = {
-      auction_id: ['AuctionID',"String"],
-      seller_id: ['Seller/Id',"String"],
-      auction_item_url: ['AuctionItemUrl',"String"],
-      image: ['Image',"String"],
-      end_time: ['EndTime',"DateTime"],
-      current_price: ['CurrentPrice',"Integer"],
-      bid_or_buy: ['BidOrBuy',"Integer"],
-      bids: ['Bids',"Integer"],
-      category_id: ['CategoryId',"Integer"],
-      title: ['Title',"String"],
-      is_reserved: ['IsReserved',"Boolean"],
+      # 格納symbol: xpath,取得proc
+      auction_id: ['AuctionID',Tag_by_str],
+      seller_id: ['Seller/Id',Tag_by_str],
+      auction_item_url: ['AuctionItemUrl',Tag_by_str],
+      image: ['Image',Tag_by_str],
+      end_time: ['EndTime',Tag_by_datetime],
+      current_price: ['CurrentPrice',Tag_by_int],
+      bid_or_buy: ['BidOrBuy',Tag_by_int],
+      bids: ['Bids',Tag_by_int],
+      category_id: ['CategoryId',Tag_by_int],
+      title: ['Title',Tag_by_str],
+      is_reserved: ['IsReserved',Tag_by_bool ],
       store: ['StoreIcon',"Boolean"], #to do 
       new_item: ['NewItemIcon',"Boolean"],
-      description: ['Description',"String"],
+      description: ['Description',Tag_by_str],
       easypayment_creditcard: ['EasyPayment/IsCreditCard',"Boolean"],
       easypayment_netbank: ['EasyPayment/IsNetBank',"Boolean"],
       charge_for_shipping: ['ChargeForShipping',"String"], # to do
-      location:['Location',"String"],
+      location: ['Location',Tag_by_str],
+      }
+
+    Tag_by_str = Proc.new { |elem,target_tag|
+        elem_part = elem.at(target_tag)
+        elem_part ? elem_part.inner_text : nil
     }
+    
+    Tag_by_int = Proc.new { |elem,target_tag|
+      tag_str = Tag_by_str.call(elem,target_tag)
+      tag_str ? tag_str.to_i : nil
+    }
+
+    Tag_has_url = Proc.new { |elem,target_tag|
+      tag_str = Tag_by_str.call(elem,target_tag)
+      tag_str ? (tag_str =~ /http/) : nil
+    }
+
+    Tag_by_datetime = Proc.new {|elem,target_tag|
+      tag_str = Tag_by_str.call(elem,target_tag)
+      tag_str ? DateTime.parse(tag_str) : nil
+    }
+
+    Tag_by_bool = Proc.new { |elem,target_tag|
+      tag_str = Tag_by_str.call(elem,target_tag)
+      tag_str ? (tag_str=="true") : nil
+    }
+
+
+  
+
+
+
+    
 
   attr_accessor :attrs,:info_when_get
 

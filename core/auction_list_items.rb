99176c7d6181b009a49845e7c929109326ffecf0
ri <git@tnu.jp>
2013-09-24 13:06:34 +0900

    attrsのタグ名を変更

--- core/auction_list_items.rb
+++ core/auction_list_items.rb
@@ -24,17 +24,17 @@ class AuctionListItems < ListItems
       charity_percent: ['CharityOption/Proportion',Tag_by_int],
       affiliate_rate: ['Affiliate/Rate',Tag_by_int],
 
-      new_sale: ['NewIcon',Tag_has_url],
+      :'new_sale?' => ['NewIcon',Tag_has_url],
       :'store?' => ['StoreIcon',Tag_has_url],  
-      checked: ['CheckIcon',Tag_has_url],  
-      public: ['PublicIcon',Tag_has_url],  
-      featured: ['FeaturedIcon',Tag_has_url],  
+      :'checked?' => ['CheckIcon',Tag_has_url],  
+      :'public?' => ['PublicIcon',Tag_has_url],  
+      :'featured?' => ['FeaturedIcon',Tag_has_url],  
       :'free_shipping?' => ['FreeshippingIcon',Tag_has_url],  
       item_condition: ['NewItemIcon',proc{|elem,tag|
 	  Tag_has_url.call(elem,tag) ? "new" : "not_new"
       } ],
-      wrapping: ['WrappingIcon',Tag_has_url],
-      easypayment: ['EasyPaymentIcon',Tag_has_url],
+      :'wrapping?' => ['WrappingIcon',Tag_has_url],
+      :'easypayment?' => ['EasyPaymentIcon',Tag_has_url],
       :'has_offer?' => ['IsOffer',Tag_by_bool],
       :'adult?' => ['IsAdult',Tag_by_bool],
   }
